95a9fd6a73b74776e9819785b7319bb0b68e43ce
vadimcimbalyk <vadymonua@gmail.com>
2022-03-07 13:17:00 +0200

    section-8 styles

--- src/App.vue
+++ src/App.vue
@@ -293,66 +293,93 @@
       </section>
       <section class="section-7">
         <div class="flex flex-col">
-          <div>
-            <span>an unique marketing template</span>
-            <p>
+          <div
+            class="triangle-bg w-1/2 mx-auto h-96 flex flex-col justify-center text-center items-center"
+          >
+            <span class="pb-6 text-xs text-sky-500"
+              >an unique marketing template</span
+            >
+            <p class="pb-8 text-2xl">
               "Any strategy that gets a message to rise above the clutter is
               terrific from the advertiser's perspective. Even if it's not much
               of a secret, it's still a pretty good marketing idea."
             </p>
-            <v-button text="Meet the team" class="text-blue-100 bg-blue-800" />
+            <v-button
+              text="Meet the team"
+              class="text-stone-50 bg-blue-800 max-w-max text-sm"
+            />
           </div>
-          <div>
-            <ul class="flex flex-row">
-              <li>
-                <img src="./assets/images/sec-7/sec-7_img-1.png" alt="" />
-              </li>
-              <li>
-                <img src="./assets/images/sec-7/sec-7_img-2.png" alt="" />
-              </li>
-              <li>
-                <img src="./assets/images/sec-7/sec-7_img-3.png" alt="" />
-              </li>
-              <li>
-                <img src="./assets/images/sec-7/sec-7_img-4.png" alt="" />
-              </li>
-              <li>
-                <img src="./assets/images/sec-7/sec-7_img-5.png" alt="" />
-              </li>
-              <li>
-                <img src="./assets/images/sec-7/sec-7_img-6.png" alt="" />
-              </li>
-            </ul>
+          <div
+            class="flex flex-row justify-between mb-14 flex-wrap border-y-2 border-slate-100 py-10"
+          >
+            <div>
+              <img src="./assets/images/sec-7/sec-7_img-1.png" alt="" />
+            </div>
+            <div>
+              <img src="./assets/images/sec-7/sec-7_img-2.png" alt="" />
+            </div>
+            <div>
+              <img src="./assets/images/sec-7/sec-7_img-3.png" alt="" />
+            </div>
+            <div>
+              <img src="./assets/images/sec-7/sec-7_img-4.png" alt="" />
+            </div>
+            <div>
+              <img src="./assets/images/sec-7/sec-7_img-5.png" alt="" />
+            </div>
+            <div>
+              <img src="./assets/images/sec-7/sec-7_img-6.png" alt="" />
+            </div>
           </div>
         </div>
       </section>
       <section class="section-8">
-        <span>FROM OUR BLOG</span>
-        <h2>Recent News & Updates</h2>
-        <div class="flex">
-          <div>
-            <div>
-              <img src="./assets/images/sec-8/sec-8_img-1.png" alt="photo" />
-              <span>DESIGN</span>
-              <h3>Best Blogs To Follow About Web Design</h3>
-              <p>
-                Et magni et ea unde tempore. Corrupti quia adipisci qui dicta
-                sint.
-              </p>
+        <div class="text-center mb-12">
+          <span class="pb-6 text-xs text-sky-500">FROM OUR BLOG</span>
+          <h2>Recent News & Updates</h2>
+        </div>
+        <div class="flex flex-row gap-4 grid-cols-2">
+          <div class="basis-1/2 grid gap-4 grid-cols-1">
+            <div
+              class="border-solid border-2 border-slate-100 b-radius p-6 flex flex-row"
+            >
+              <div class="basis-1/2 mr-8">
+                <img src="./assets/images/sec-8/sec-8_img-1.png" alt="photo" />
+              </div>
+              <div class="basis-1/2">
+                <span class="text-xs text-sky-500">DESIGN</span>
+                <h4 class="mb-6">Best Blogs To Follow About Web Design</h4>
+                <p class="text-xs">
+                  Et magni et ea unde tempore. Corrupti quia adipisci qui dicta
+                  sint.
+                </p>
+              </div>
             </div>
-            <div>
-              <img src="./assets/images/sec-8/sec-8_img-2.png" alt="photo" />
-              <span>TUTORIAL</span>
-              <h3>5 Great Web Design Resources</h3>
-              <p>
-                Et magni et ea unde tempore. Corrupti quia adipisci qui dicta
-                sint.
-              </p>
+            <div
+              class="border-solid border-2 border-slate-100 p-6 flex flex-row b-radius"
+            >
+              <div class="basis-1/2 mr-8">
+                <img src="./assets/images/sec-8/sec-8_img-2.png" alt="photo" />
+              </div>
+              <div class="basis-1/2">
+                <span class="text-xs text-sky-500">TUTORIAL</span>
+                <h4 class="mb-6">5 Great Web Design Resources</h4>
+                <p class="text-xs">
+                  Et magni et ea unde tempore. Corrupti quia adipisci qui dicta
+                  sint.
+                </p>
+              </div>
             </div>
           </div>
-          <div>
-            <span>MARKETING</span>
-            <h3>What Will Website Be Like In 100 Years?</h3>
+          <div
+            class="basis-1/2 p-6 border-solid border-2 border-slate-100 b-radius overflow-hidden"
+          >
+            <div
+              class="marketing h-full flex flex-col justify-end p-10 b-radius"
+            >
+              <span class="text-xs text-sky-500">MARKETING</span>
+              <h3>What Will Website Be Like In 100 Years?</h3>
+            </div>
           </div>
         </div>
       </section>
