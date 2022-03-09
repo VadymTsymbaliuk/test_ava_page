386bb1bde4e4d2fb6f390fb7f1a94617d2b9dddf
vadimcimbalyk <vadymonua@gmail.com>
2022-03-09 14:14:58 +0200

    carousel

--- src/components/VCarousel.vue
+++ src/components/VCarousel.vue
@@ -1,38 +1,141 @@
 <template>
-  <VueSlickCarousel v-bind="settings">
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-  </VueSlickCarousel>
+  <div class="slider" ref="slider">
+    <div
+      class="b-radius border-solid border-2 border-slate-100 mr-2 p-12 text-center"
+    >
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. "
+      </p>
+      <img
+        src="../assets/images/sec-6/sec-6_img-1.png"
+        alt="img-1"
+        class="pb-4"
+      />
+      <h4>Martin Eduard</h4>
+      <p>CEO of Marketing</p>
+      <img src="../assets/images/sec-6/stars.png" alt="stars" />
+    </div>
+    <div
+      class="b-radius border-solid border-2 border-slate-100 mr-2 p-12 text-center"
+    >
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. "
+      </p>
+      <img
+        src="../assets/images/sec-6/sec-6_img-2.png"
+        alt="img-1"
+        class="pb-4"
+      />
+      <h4>Martin Eduard</h4>
+      <p>CEO of Marketing</p>
+      <img src="../assets/images/sec-6/stars.png" alt="stars" />
+    </div>
+    <div
+      class="b-radius border-solid border-2 border-slate-100 mr-2 p-12 text-center"
+    >
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. "
+      </p>
+      <img
+        src="../assets/images/sec-6/sec-6_img-1.png"
+        alt="img-1"
+        class="pb-4"
+      />
+      <h4>Martin Eduard</h4>
+      <p>CEO of Marketing</p>
+      <img src="../assets/images/sec-6/stars.png" alt="stars" />
+    </div>
+    <div
+      class="b-radius border-solid border-2 border-slate-100 mr-2 p-12 text-center"
+    >
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. "
+      </p>
+      <img
+        src="../assets/images/sec-6/sec-6_img-1.png"
+        alt="img-1"
+        class="pb-4"
+      />
+      <h4>Martin Eduard</h4>
+      <p>CEO of Marketing</p>
+      <img src="../assets/images/sec-6/stars.png" alt="stars" />
+    </div>
+    <div
+      class="b-radius border-solid border-2 border-slate-100 mr-2 p-12 text-center"
+    >
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. "
+      </p>
+      <img
+        src="../assets/images/sec-6/sec-6_img-1.png"
+        alt="img-1"
+        class="pb-4"
+      />
+      <h4>Martin Eduard</h4>
+      <p>CEO of Marketing</p>
+      <img src="../assets/images/sec-6/stars.png" alt="stars" />
+    </div>
+    <div
+      class="b-radius border-solid border-2 border-slate-100 mr-2 p-12 text-center"
+    >
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. "
+      </p>
+      <img
+        src="../assets/images/sec-6/sec-6_img-1.png"
+        alt="img-1"
+        class="pb-4"
+      />
+      <h4>Martin Eduard</h4>
+      <p>CEO of Marketing</p>
+      <img src="../assets/images/sec-6/stars.png" alt="stars" />
+    </div>
+  </div>
 </template>
 
 <script>
-import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
-import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import $ from "jquery";
 
 export default {
   name: "VCarousel",
-  props: {
-    components: { VueSlickCarousel },
-    data: () => ({
-      settings: {
-        dots: true,
-        dotsClass: "slick-dots custom-dot-class",
-        edgeFriction: 0.35,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    }),
+  mounted() {
+    $(".slider").slick({
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 300,
+          settings: "unslick", // destroys slick
+        },
+      ],
+    });
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.slider img {
+  display: initial;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/scss/index.scss";
 import "tw-elements";
+import "jquery/dist/jquery.min.js";
+import "slick-carousel/slick/slick.min";
+import "slick-carousel/slick/slick.scss";
+import "slick-carousel/slick/slick-theme.scss";
 
 createApp(App).mount("#app");
