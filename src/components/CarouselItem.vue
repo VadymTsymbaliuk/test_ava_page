f29d089fbe03a190c0a85a270798d52d492e4e71
vadimcimbalyk <vadymonua@gmail.com>
2022-03-09 12:32:10 +0200

    carousel

---
+++ src/components/CarouselItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="carousel">
+    <p><slot></slot></p>
+    <img :src="slide" alt="" />
+    <h4><slot></slot></h4>
+    <p><slot></slot></p>
+    <img src="./assets/images" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselItem",
+};
+</script>
+
+<style scoped></style>
